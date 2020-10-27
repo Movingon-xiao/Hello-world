5c8a1d434680995f7f9dbbfc0480b76efb45091b
Movingon-xiao <73449715+Movingon-xiao@users.noreply.github.com>
2020-10-27 09:39:28 +0800

    Add files via upload

---
+++ kNN.py
@@ -0,0 +1,30 @@
+from numpy import *
+import operator
+
+
+def createDataSet():
+    group = array([[1.1, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    """距离计算"""
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortdClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortdClassCount[0][0]
+
+
+group, labels = createDataSet()
+result = classify0([0, 3], group, labels, 3)
+print(result)
